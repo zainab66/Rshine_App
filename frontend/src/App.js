370f396a067218b64fa5c1009d40d93fa52c426a
zainab66 <zainab66r@gmail.com>
2020-11-27 00:54:48 -0500

    Display Products dynamically and display rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,132 +1,45 @@
 import React from 'react';
-
+import data from './data';
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
+    <div className="grid-container">
+    <header className="row">
       <div>
-        <a class="brand" href="index.html">Rshine</a>
+        <a className="brand" href="/">Rshine</a>
       </div>
       <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
       </div>
     </header>
     <main>
       <div>
-        <div class="row center">
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
+        <div className="row center">
+          {data.products.map(product => ( 
+          <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+              <img className="medium" src={product.image} alt={product.name} />
             </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
+            <div className="card-body">
+            <a href={`/product/${product._id}`}>
+                <h2>{product.name}</h2>
               </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
               </div>
-              <div class="price">CA$12.74</div>
+          <div className="price">CA${product.price}</div>
             </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
-              </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-              </div>
-              <div class="price">CA$12.74</div>
-            </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
-              </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-              </div>
-              <div class="price">CA$12.74</div>
-            </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
-              </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-              </div>
-              <div class="price">CA$12.74</div>
-            </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
-              </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-              </div>
-              <div class="price">CA$12.74</div>
-            </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>Molang Birthday Cake Topper </h2>
-              </a>
-              <div class="rating">
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star"></i> </span>
-                <span> <i class="fa fa-star-half-o"></i> </span>
-              </div>
-              <div class="price">CA$12.74</div>
-            </div>
-          </div>
+          </div>) 
+          )}
+         
         </div>
       </div>
     </main>
-    <footer class="row center">All right reserved</footer>
+    <footer className="row center">All right reserved</footer>
   </div>
   );
 }
