396d0f7ea0b92a12f0d67138407b86f33a61965d
zainab66 <zainab66r@gmail.com>
2020-11-30 12:53:04 -0500

    create product details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+import { BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="row">
       <div>
@@ -15,16 +19,14 @@ function App() {
       </div>
     </header>
     <main>
-      <div>
-        <div className="row center">
-          {data.products.map((product) => ( 
-          <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-      </div>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+
+      <Route path="/" component={HomeScreen} exact></Route>
+     
     </main>
     <footer className="row center">All right reserved</footer>
   </div>
+  </BrowserRouter>
   );
 }
 
