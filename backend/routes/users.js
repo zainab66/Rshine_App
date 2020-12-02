e72716360a771abd70fc28e42edf378151c17583
zainab66 <zainab66r@gmail.com>
2020-12-02 15:10:32 -0500

    Create Signin Backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data.js');
 const User  = require('../models/userModel.js');
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils.js');
 
 const expressAsyncHandler = require('express-async-handler');
 
@@ -14,4 +16,31 @@ router.get(
     res.send({ createdUsers });
   })
 );
+
+router.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: jwtGenerator(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+
+
+
+
+
+
 module.exports = router;
